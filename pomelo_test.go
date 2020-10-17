54346c0618d0859cab585f55d3ef770829429a5d
Zh1cheung <zh1cheunglq@gmail.com>
2020-10-17 22:32:08 +0800

    Update pomelo_test.go

--- pomelo_test.go
+++ pomelo_test.go
@@ -26,4 +26,65 @@ func TestVector1(t *testing.T) {
 		{"supersecretkeyyoushouldnotcommit", "0102030405060708090a0b0c0102030405060708090a0b0c", 123206400, "Hello world!", "875GH233T7IYrxtgXxlQBYiFobZMQdHAT51vChKsAIYCFxZtL1evV54vYqLyZtQ0ekPHt8kJHQp0a"},
 	}
 
+	for _, table := range testVectors {
+		b := NewPomelo(table.key)
+		b.setRandom(table.random)
+		b.setTimeStamp(table.timestamp)
+
+		// Encode string.
+		encoded, err := b.EncodeToString(table.payload)
+		if err != nil {
+			t.Errorf("%q", err)
+		}
+		if encoded != table.expected {
+			t.Errorf("EncodeToString(\"%s\") = %s. got %s, expected %q", table.payload, encoded, encoded, table.expected)
+		}
+
+		// Decode string.
+		decoded, err := b.DecodeToString(encoded)
+		if err != nil {
+			t.Errorf("%q", err)
+		}
+		if decoded != table.payload {
+			t.Errorf("DecodeToString(\"%s\") = %s. got %s, expected %q", table.expected, decoded, decoded, table.expected)
+		}
+	}
+}
+
+// TestVector2 for testing encoding data to a valid pomelo token with a TTL.
+func TestVector2(t *testing.T) {
+	testVectors = []struct {
+		key       string
+		random    string
+		timestamp uint32
+		payload   string
+		expected  string
+	}{
+		{"supersecretkeyyoushouldnotcommit", "0102030405060708090a0b0c0102030405060708090a0b0c", 123206400, "Hello world!", "875GH233T7IYrxtgXxlQBYiFobZMQdHAT51vChKsAIYCFxZtL1evV54vYqLyZtQ0ekPHt8kJHQp0a"},
+	}
+
+	for _, table := range testVectors {
+		b := NewPomelo(table.key)
+		b.setRandom(table.random)
+		b.setTimeStamp(table.timestamp)
+
+		// Encode string.
+		encoded, err := b.EncodeToString(table.payload)
+		if err != nil {
+			t.Errorf("%q", err)
+		}
+		if encoded != table.expected {
+			t.Errorf("EncodeToString(\"%s\") = %s. got %s, expected %q", table.payload, encoded, encoded, table.expected)
+		}
+
+		// Decode string with TTL. Should throw an error with no token encoded because it has expired.
+		b.SetTTL(3600)
+		decoded, derr := b.DecodeToString(encoded)
+		if derr == nil {
+			t.Errorf("%q", derr)
+		}
+		if decoded != "" {
+			t.Errorf("DecodeToString(\"%s\") = %s. got %s, expected %q", table.expected, decoded, decoded, table.expected)
+		}
+	}
 }
