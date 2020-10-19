6c2b1df8a34fa645d4b4c9b314be7240cc809dce
Zh1cheung <zh1cheunglq@gmail.com>
2020-10-19 14:37:40 +0800

    add test func

--- pomelo_test.go
+++ pomelo_test.go
@@ -143,3 +143,63 @@ func TestInvalidEncodeString(t *testing.T) {
 		}
 	}
 }
+
+// TestInvalidDecodeString for testing errors when decoding pomelo tokens.
+func TestInvalidDecodeString(t *testing.T) {
+	testVectors = []struct {
+		key       string
+		random    string
+		timestamp uint32
+		payload   string
+		expected  string
+	}{
+		{"supersecretkeyyoushouldnotcommit", "0102030405060708090a0b0c0102030405060708090a0b0c", 123206400, "Hello world!", "875GH233T7IYrxtgXxlQBYiFobZMQdHAT51vChKsAIYCFxZtL1evV54vYqLyZtQ0ekPHt8kJHQp0"}, // Invalid base62
+
+		{"supersecretkeyyoushouldnotcommi", "", 123206400, "Hello world!",
+			"875GH233T7IYrxtgXxlQBYiFobZMQdHAT51vChKsA"}, // Invalid key + Invalid base62.
+
+		{"supersecretkeyyoushouldnotcommi", "0102030405060708090a0b0c0102030405060708090a0b0c", 123206400, "Hello world!", "875GH233T7IYrxtgXxlQBYiFobZMQdHAT51vChKsAIYCFxZtL1evV54vYqLyZtQ0ekPHt8kJHQp0a"}, // Invalid key
+
+		{"supersecretkeyyoushouldnotcommit", "0102030405060708090a0b0c0102030405060708090a0b0c", 123206400, "Hello world!", "875GH233T7IYrxtgXxlQBYiFobZMQdHAT51vChKsAIYCFxZtL1evV54vYqLOZtQ0ekPHt8kJHQp0a"}, // Invalid malformed base62
+	}
+
+	for _, table := range testVectors {
+		b := NewPomelo(table.key)
+
+		_, err := b.DecodeToString(table.expected)
+		if err == nil {
+			t.Errorf("%q", err)
+		}
+	}
+}
+
+// TestExpiredTokenError tests if decoding an expired tokens returns the corresponding error type.
+func TestExpiredTokenError(t *testing.T) {
+	b := NewPomelo("supersecretkeyyoushouldnotcommit")
+
+	ttl := time.Second * 1
+	b.SetTTL(uint32(ttl.Seconds()))
+	token, encErr := b.EncodeToString("Hello World!")
+	if encErr != nil {
+		t.Errorf("%q", encErr)
+	}
+
+	// Wait (with enough additional waiting time) until the token is expired...
+	time.Sleep(ttl * 3)
+	// ...and decode the token again that is expired by now.
+	_, decErr := b.DecodeToString(token)
+	var errExpiredToken *ErrExpiredToken
+	if !errors.As(decErr, &errExpiredToken) {
+		t.Errorf("%v", decErr)
+	}
+}
+
+// TestInvalidTokenError tests if decoding an invalid token returns the corresponding error type.
+func TestInvalidTokenError(t *testing.T) {
+	b := NewPomelo("supersecretkeyyoushouldnotcommit")
+
+	_, err := b.DecodeToString("$")
+	if !errors.Is(err, ErrInvalidToken) {
+		t.Errorf("%v", err)
+	}
+}
